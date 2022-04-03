f7f132a4b8b60e0d54c9a4b7715511e2a7fea032
Andrey Zhdanov <andrei_zhdanov@epam.com>
2022-04-03 11:42:16 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# epam-tf-practice
---
+++ task1/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+
+data "aws_vpcs" "some-project-vpcs" {}
+
+data "aws_subnets" "some-project-subnets" {
+  filter {
+    name   = "vpc-id"
+    values = data.aws_vpcs.some-project-vpcs.ids
+  }
+}
+
+data "aws_security_groups" "some-project-sgs" {
+  filter {
+    name   = "vpc-id"
+    values = data.aws_vpcs.some-project-vpcs.ids
+  }
+}
+
+
+output "some-project-vpcs" {
+  value = data.aws_vpcs.some-project-vpcs.ids
+}
+
+output "some-project-subnets" {
+  value = data.aws_subnets.some-project-subnets.ids
+}
+
+output "some-project-sgs" {
+  value = data.aws_security_groups.some-project-sgs.ids
+}
